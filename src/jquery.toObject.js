fedd1e7716fdc012b4f29518f9c76eae34a3247f
Benjamin Besse <contact@throrinstudio.com>
2016-02-04 17:16:31 +0100

    Fix browserify problem with jquery.toObject

--- src/jquery.toObject.js
+++ src/jquery.toObject.js
@@ -24,7 +24,34 @@
  * Time: 20:09
  */
 
-(function($){
+(function(factory) {
+
+    // Establish the root object, `window` (`self`) in the browser, or `global` on the server.
+    // We use `self` instead of `window` for `WebWorker` support.
+    var root = (typeof self == 'object' && self.self == self && self) ||
+        (typeof global == 'object' && global.global == global && global);
+
+    // Set up Backbone appropriately for the environment. Start with AMD.
+    if (typeof define === 'function' && define.amd) {
+        define(['form2js', 'jquery'], function(form2js, $) {
+            // Export global even in AMD case in case this script is loaded with
+            // others that may still expect a global Backbone.
+            factory(root, form2js, $);
+        });
+
+        // Next for Node.js or CommonJS. jQuery may not be needed as a module.
+    } else if (typeof exports !== 'undefined') {
+        var form2js = require('form2js').form2js, $;
+        try { $ = require('jquery'); } catch(e) {}
+
+        factory(root, form2js, $);
+
+        // Finally, as a browser global.
+    } else {
+        factory(root, root.form2js, (root.jQuery || root.Zepto || root.ender || root.$));
+    }
+
+}(function(root, form2js, $){
 
 	/**
 	 * jQuery wrapper for form2object()
@@ -63,4 +90,4 @@
 		}
 	}
 
-})(jQuery);
+}));
